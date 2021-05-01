89d0fb7aa5afbdcccda2f523f34599ff80667fa5
no803 <83288656+no803@users.noreply.github.com>
2021-05-01 23:34:38 +0200

    Fixed some bugs
    - Type Mistake 
    - Const not good.

--- commands/config.js
+++ commands/config.js
@@ -5,7 +5,7 @@ const { truncate } = require("fs");
 module.exports = {
     name: "config",
     description: "set guild anit raid config",
-    run: async (client, message, args) => {
+    async execute(client, message, args, Discord){
     let cmd = args[0];
     const guildicon = message.guild.iconURL();
     if(!cmd) {
@@ -57,16 +57,18 @@ module.exports = {
     .setFooter(message.guild.name, guildicon)
     return message.channel.send(showembed);
  }
- if(cmd.toLowerCase() === 'setrolecreatelimit') {
+
+if(cmd.toLowerCase() === 'setrolecreatelimit') {
 let rolecreate = args.slice(1).join(" ");
 if(!rolecreate) {
- let missing = new Discord.MessageEmbed()
- .setAuthor(message.author.username, message.author.displayAvatarURL())
- .setDescription(`** an invaild usage**\nconfig setrolecreatelimit (number)`)
- .setFooter(message.guild.name, guildicon)
+let missing = new Discord.MessageEmbed()
+.setAuthor(message.author.username, message.author.displayAvatarURL())
+.setDescription(`** an invaild usage**\nconfig setrolecreatelimit (number)`)
+.setFooter(message.guild.name, guildicon)
 
-  return message.channel.send(missing);
+ return message.channel.send(missing);
 }
+
 if(isNaN(rolecreate)) {
   let missing = new Discord.MessageEmbed()
   .setAuthor(message.author.username, message.author.displayAvatarURL())
@@ -74,82 +76,86 @@ if(isNaN(rolecreate)) {
   .setFooter(message.guild.name, guildicon)
 return message.channel.send(missing);
 }
+
 db.set(`rolecreatelimit_${message.guild.id}`, rolecreate)
-let done = new Discord.MessageEmbed() 
+const done = new Discord.MessageEmbed() 
 .setAuthor(message.author.username, message.author.displayAvatarURL())
 .setDescription(`Done SetRoleCreation limits Has Been Set To ${rolecreate} ✅`)
 .setFooter(message.guild.name, guildicon)
+
 return message.channel.send(done);
 }
-if(cmd.toLowerCase() === 'setroledeletelimit') {
-  let roledelete = args.slice(1).join(" ");
-  if(!roledelete) {
-   let missing = new Discord.MessageEmbed()
-   .setAuthor(message.author.username, message.author.displayAvatarURL())
-   .setDescription(`** an invaild usage**\nconfig setroledeletelimit (number)`)
-   .setFooter(message.guild.name, guildicon)
-  
+    }
+  }
+  if(cmd.toLowerCase() === 'setroledeletelimit') {
+    let roledelete = args.slice(1).join(" ");
+    if(!roledelete) {
+     let missing = new Discord.MessageEmbed()
+     .setAuthor(message.author.username, message.author.displayAvatarURL())
+     .setDescription(`** an invaild usage**\nconfig setroledeletelimit (number)`)
+     .setFooter(message.guild.name, guildicon)
+    
+      return message.channel.send(missing);
+    }
+    if(isNaN(roledelete)) {
+      let missing = new Discord.MessageEmbed()
+      .setAuthor(message.author.username, message.author.displayAvatarURL())
+      .setDescription(`** an invaild usage (Cannot be words only numbers)**\nconfig setroledeletelimit (number)`)
+      .setFooter(message.guild.name, guildicon)
     return message.channel.send(missing);
-  }
-  if(isNaN(rolecreate)) {
-    let missing = new Discord.MessageEmbed()
+    }
+    db.set(`roledeletelimits_${message.guild.id}`, rolecreate)
+    let done = new Discord.MessageEmbed() 
     .setAuthor(message.author.username, message.author.displayAvatarURL())
-    .setDescription(`** an invaild usage (Cannot be words only numbers)**\nconfig setroledeletelimit (number)`)
+    .setDescription(`Done SetRoleDelete limits Has Been Set To ${rolecreate} ✅`)
     .setFooter(message.guild.name, guildicon)
-  return message.channel.send(missing);
+    return message.channel.send(done);
   }
-  db.set(`roledeletelimits_${message.guild.id}`, rolecreate)
-  let done = new Discord.MessageEmbed() 
-  .setAuthor(message.author.username, message.author.displayAvatarURL())
-  .setDescription(`Done SetRoleDelete limits Has Been Set To ${rolecreate} ✅`)
-  .setFooter(message.guild.name, guildicon)
-  return message.channel.send(done);
-  
-}
-if(cmd.toLowerCase() === 'setactionlogs') {
-  let logs = message.mentions.channels.first();
-  if(!logs) {
-  let logsembed = new Discord.MessageEmbed()
-  .setAuthor(message.author.username, message.author.displayAvatarURL())
-  .setDescription(`Please Mention an vaild channel`)
-  .setFooter(message.guild.name, guildicon)
-return message.channel.send(logsembed);
-}
-logs.send(`** Anit-Raid Logs Room **`)
-db.set(`acitonslogs_${message.guild.id}`, logs.id)
-let done = new Discord.MessageEmbed()
-.setAuthor(message.author.username, message.author.displayAvatarURL())
-.setDescription(`well done aciton-logs channel has been set to ${logs}`)
-.setFooter(message.guild.name, guildicon)
-return message.channel.send(done)
-}
-if(cmd.toLowerCase() === 'setchannelcreatelimit') {
-  let rolecreate = args.slice(1).join(" ");
-  if(!rolecreate) {
-   let missing = new Discord.MessageEmbed()
-   .setAuthor(message.author.username, message.author.displayAvatarURL())
-   .setDescription(`** an invaild usage**\nconfig setchannelcreatelimit (number)`)
-   .setFooter(message.guild.name, guildicon)
-  
-    return message.channel.send(missing);
-  }
-  if(isNaN(rolecreate)) {
-    let missing = new Discord.MessageEmbed()
+  if(cmd.toLowerCase() === 'setactionlogs') {
+    let logs = message.mentions.channels.first();
+    if(!logs) {
+    let logsembed = new Discord.MessageEmbed()
     .setAuthor(message.author.username, message.author.displayAvatarURL())
-    .setDescription(`** an invaild usage (Cannot be words only numbers)**\nconfig setchannelcreatelimit (number)`)
+    .setDescription(`Please Mention an vaild channel`)
     .setFooter(message.guild.name, guildicon)
-  return message.channel.send(missing);
+  return message.channel.send(logsembed);
   }
-  db.set(`channelcreatelimits_${message.guild.id}`, rolecreate)
-  let done = new Discord.MessageEmbed() 
+  logs.send(`** Anit-Raid Logs Room **`)
+  db.set(`acitonslogs_${message.guild.id}`, logs.id)
+  let done = new Discord.MessageEmbed()
   .setAuthor(message.author.username, message.author.displayAvatarURL())
-  .setDescription(`Done Channel Create limits Has Been Set To ${rolecreate} ✅`)
+  .setDescription(`well done aciton-logs channel has been set to ${logs}`)
   .setFooter(message.guild.name, guildicon)
-  return message.channel.send(done);
-}
-if(cmd.toLowerCase() === 'setchanneldeletelimit') {
-  let rolecreate = args.slice(1).join(" ");
-  if(!rolecreate) {
+  return message.channel.send(done)
+  }
+  if(cmd.toLowerCase() === 'setchannelcreatelimit') {
+    let channelcreate = args.slice(1).join(" ");
+    if(!channelcreate) {
+     let missing = new Discord.MessageEmbed()
+     .setAuthor(message.author.username, message.author.displayAvatarURL())
+     .setDescription(`** an invaild usage**\nconfig setchannelcreatelimit (number)`)
+     .setFooter(message.guild.name, guildicon)
+    
+      return message.channel.send(missing);
+    }
+    if(isNaN(channelcreate)) {
+      let missing = new Discord.MessageEmbed()
+      .setAuthor(message.author.username, message.author.displayAvatarURL())
+      .setDescription(`** an invaild usage (Cannot be words only numbers)**\nconfig setchannelcreatelimit (number)`)
+      .setFooter(message.guild.name, guildicon)
+   
+      return message.channel.send(missing);
+    }
+    db.set(`channelcreatelimits_${message.guild.id}`, rolecreate)
+    let done = new Discord.MessageEmbed() 
+    .setAuthor(message.author.username, message.author.displayAvatarURL())
+    .setDescription(`Done Channel Create limits Has Been Set To ${rolecreate} ✅`)
+    .setFooter(message.guild.name, guildicon)
+   
+    return message.channel.send(done);
+}if(cmd.toLowerCase() === 'setchanneldeletelimit') {
+  let channeldelete = args.slice(1).join(" ");
+  if(!channeldelete) {
    let missing = new Discord.MessageEmbed()
    .setAuthor(message.author.username, message.author.displayAvatarURL())
    .setDescription(`** an invaild usage**\nconfig setchanneldeletelimit (number)`)
@@ -157,7 +163,7 @@ if(cmd.toLowerCase() === 'setchanneldeletelimit') {
   
     return message.channel.send(missing);
   }
-  if(isNaN(rolecreate)) {
+  if(isNaN(channeldelete)) {
     let missing = new Discord.MessageEmbed()
     .setAuthor(message.author.username, message.author.displayAvatarURL())
     .setDescription(`** an invaild usage (Cannot be words only numbers)**\nconfig setchanneldeletelimit (number)`)
@@ -172,8 +178,8 @@ if(cmd.toLowerCase() === 'setchanneldeletelimit') {
   return message.channel.send(done);
 }
 if(cmd.toLowerCase() === 'setbanlimits') {
-  let rolecreate = args.slice(1).join(" ");
-  if(!rolecreate) {
+  let banlimit = args.slice(1).join(" ");
+  if(!banlimit) {
    let missing = new Discord.MessageEmbed()
    .setAuthor(message.author.username, message.author.displayAvatarURL())
    .setDescription(`** an invaild usage**\nconfig setbanlimit (number)`)
@@ -181,7 +187,7 @@ if(cmd.toLowerCase() === 'setbanlimits') {
   
     return message.channel.send(missing);
   }
-  if(isNaN(rolecreate)) {
+  if(isNaN(banlimit)) {
     let missing = new Discord.MessageEmbed()
     .setAuthor(message.author.username, message.author.displayAvatarURL())
     .setDescription(`** an invaild usage (Cannot be words only numbers)**\nconfig setbanlimit (number)`)
@@ -196,8 +202,8 @@ if(cmd.toLowerCase() === 'setbanlimits') {
   return message.channel.send(done);
 }
 if(cmd.toLowerCase() === 'setkicklimits') {
-  let rolecreate = args.slice(1).join(" ");
-  if(!rolecreate) {
+  let kicklimit = args.slice(1).join(" ");
+  if(!kicklimit) {
    let missing = new Discord.MessageEmbed()
    .setAuthor(message.author.username, message.author.displayAvatarURL())
    .setDescription(`** an invaild usage**\nconfig setbanlimit (number)`)
@@ -205,7 +211,7 @@ if(cmd.toLowerCase() === 'setkicklimits') {
   
     return message.channel.send(missing);
   }
-  if(isNaN(rolecreate)) {
+  if(isNaN(kicklimit)) {
     let missing = new Discord.MessageEmbed()
     .setAuthor(message.author.username, message.author.displayAvatarURL())
     .setDescription(`** an invaild usage (Cannot be words only numbers)**\nconfig setkicklimits (number)`)
@@ -232,5 +238,4 @@ db.delete(`executer_${message.guild.id}_${user.id}_channelcreate`)
 db.delete(`executer_${message.guild.id}_${user.id}_channeldelete`)
 return message.channel.send(`Reseted User limits`);
 }
-}}
  
